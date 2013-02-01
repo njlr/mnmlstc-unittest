7a3f6ae6a21483c461b147823c793a69976bf10b
Tres Walsh <tres.walsh@mnmlstc.com>
2013-02-01 12:02:39 -0800

    Add unittest::test interface header (unittest/test.hpp)

---
+++ include/unittest/test.hpp
@@ -0,0 +1,33 @@
+#ifndef UNITTEST_TEST_HPP
+#define UNITTEST_TEST_HPP
+#pragma once
+
+#include <initializer_list>
+#include <string>
+
+#include <unittest/export.hpp>
+
+namespace unittest {
+inline namespace v1 {
+
+class UNITTEST_EXPORT_API test final {
+  std::string label;
+
+  auto operator = (test const&) noexcept -> test& = delete;
+  auto operator = (test&&) noexcept -> test& = delete;
+
+  test () noexcept = delete;
+
+public:
+  test (test const&) noexcept;
+  test (test&&) noexcept;
+
+  explicit test (const char*);
+
+  auto operator = (std::initializer_list<class step> steps) noexcept -> test&;
+  auto name () const noexcept -> std::string const&;
+};
+
+}} /* namespace unittest::v1 */
+
+#endif /* UNITTEST_TEST_HPP */
