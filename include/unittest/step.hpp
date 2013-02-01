6aa5d774f2c26878919bbff67d152a79062469f8
Tres Walsh <tres.walsh@mnmlstc.com>
2013-02-01 12:14:03 -0800

    Add unittest::step interface header (unittest/step.hpp)

---
+++ include/unittest/step.hpp
@@ -0,0 +1,36 @@
+#ifndef MNMLSTC_UNITTEST_STEP_HPP
+#define MNMLSTC_UNITTEST_STEP_HPP
+#pragma once
+
+#include <functional>
+#include <string>
+
+#include <unittest/export.hpp>
+
+namespace unittest {
+inline namespace v1 {
+
+class UNITTEST_EXPORT_API step final {
+  std::function<void(void)> function;
+  std::string label;
+
+  auto operator = (step const&) noexcept -> step& = delete;
+  auto operator = (step&&) noexcept -> step& = delete;
+
+  step () noexcept = delete;
+
+public:
+  step (step const&) noexcept;
+  step (step&&) noexcept;
+
+  explicit step (const char*) noexcept;
+
+  auto operator = (std::function<void(void)> function) noexcept -> step&;
+  auto operator () () const -> void;
+
+  auto name () const noexcept -> std::string const&;
+};
+
+}} /* namespace unittest::v1 */
+
+#endif /* MNMLSTC_UNITTEST_STEP_HPP */
