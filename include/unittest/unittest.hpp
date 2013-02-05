c82b158fcc41c1d79427a89bc666a827c5ea2791
Tres Walsh <tres.walsh@mnmlstc.com>
2013-02-04 16:15:33 -0800

    Add unittest amalgation header (unittest/unittest.hpp)

---
+++ include/unittest/unittest.hpp
@@ -0,0 +1,10 @@
+#ifndef UNITTEST_AMALGATION_HPP
+#define UNITTEST_AMALGATION_HPP
+#pragma once
+
+#include <unittest/configure.hpp>
+#include <unittest/export.hpp>
+#include <unittest/test.hpp>
+#include <unittest/step.hpp>
+
+#endif /* UNITTEST_AMALGATION_HPP */
