267cebacbff7763827a73bd99c82d22a9f54aca3
Tres Walsh <tres.walsh@mnmlstc.com>
2013-02-07 11:59:29 -0800

    Add unittest/skip.hpp and unittest/error.hpp to the amalgation header

--- include/unittest/unittest.hpp
+++ include/unittest/unittest.hpp
@@ -4,7 +4,9 @@
 
 #include <unittest/configure.hpp>
 #include <unittest/export.hpp>
+#include <unittest/error.hpp>
 #include <unittest/test.hpp>
 #include <unittest/task.hpp>
+#include <unittest/skip.hpp>
 
 #endif /* UNITTEST_AMALGATION_HPP */
