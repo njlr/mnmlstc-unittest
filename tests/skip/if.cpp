424dac516b1e14789a066581c0bbbea40fcad85e
Tres Walsh <tres.walsh@mnmlstc.com>
2013-02-07 13:04:20 -0800

    Modify skip-unless test to have failures and passes
    Modify skip-if test variables to something a little less verbose
    
      And with that, this commit closes #5

--- tests/skip/if.cpp
+++ tests/skip/if.cpp
@@ -4,37 +4,36 @@
 #include <iostream>
 #include <csignal>
 
+/* Tests the skip_if interface */
 auto v1 () -> void {
-  auto skip_if_always = unittest::v1::skip_if(true, "always-skip") = []{};
-  auto skip_if_never = unittest::v1::skip_if(false, "never-skip") = []{
-    throw 42;
-  };
+  auto always = unittest::v1::skip_if(true, "always-skip") = []{};
+  auto never = unittest::v1::skip_if(false, "never-skip") = []{ throw 42; };
 
   try {
-    skip_if_always();
-    std::clog << "skip_if_always did not throw" << std::endl;
+    always();
+    std::clog << "skip_if always did not throw" << std::endl;
     std::exit(EXIT_FAILURE);
   } catch (unittest::v1::skip_error const& e) {
     if (e.what() != std::string { "always-skip" }) {
-      std::clog << "skip_if_always did not throw proper error" << std::endl;
+      std::clog << "skip_if always did not throw proper error" << std::endl;
       std::exit(EXIT_FAILURE);
     }
   } catch (...) {
-    std::clog << "unexpected exception thrown by skip_if_always" << std::endl;
+    std::clog << "unexpected exception thrown by skip_if always" << std::endl;
     std::exit(EXIT_FAILURE);
   }
 
   try {
-    skip_if_never();
-    std::clog << "skip_if_never does not contain proper function" << std::endl;
+    never();
+    std::clog << "skip_if never does not contain proper function" << std::endl;
     std::exit(EXIT_FAILURE);
   } catch (int value) {
     if (value != 42) {
-      std::clog << "skip_if_never function threw incorrect value" << std::endl;
+      std::clog << "skip_if never function threw incorrect value" << std::endl;
       std::exit(EXIT_FAILURE);
     }
   } catch (...) {
-    std::clog << "unexpected exception thrown by skip_if_never" << std::endl;
+    std::clog << "unexpected exception thrown by skip_if never" << std::endl;
     std::exit(EXIT_FAILURE);
   }
 }
--- tests/skip/unless.cpp
+++ tests/skip/unless.cpp
@@ -1,8 +1,50 @@
+#include <unittest/error.hpp>
 #include <unittest/skip.hpp>
 
 #include <iostream>
+#include <csignal>
+
+/* Test the skip_unless interface */
+auto v1 () -> void {
+  auto always = unittest::v1::skip_unless(false, "always-skip") = []{};
+  auto never = unittest::v1::skip_unless(true, "never-skip") = []{ throw 42; };
+
+  try {
+    always();
+    std::clog << "'always' did not throw" << std::endl;
+    std::exit(EXIT_FAILURE);
+  } catch (unittest::v1::skip_error const& e) {
+    if (e.what() != std::string { "always-skip" }) {
+      std::clog << "'always' did not throw proper error" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  } catch (...) {
+    std::clog << "unexpected exception thrown by 'always'" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try {
+    never();
+    std::clog << "'never' does not contain proper function" << std::endl;
+    std::exit(EXIT_FAILURE);
+  } catch (int value) {
+    if (value != 42) {
+      std::clog << "'never' function threw incorrect value" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  } catch (...) {
+    std::clog << "unexpected exception thrown by 'never'" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+auto sigsegv (int) -> void {
+  std::clog << "SIGSEGV signal triggered" << std::endl;
+  std::exit(EXIT_FAILURE);
+}
 
 auto main () -> int {
-  std::clog << "Not Yet Implemented" << std::endl;
-  return EXIT_FAILURE;
+  std::signal(SIGSEGV, sigsegv);
+  v1();
+  return EXIT_SUCCESS;
 }
