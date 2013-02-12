23f9ab735efdc7df9874f449cf971c0cebb561e4
Tres Walsh <tres.walsh@mnmlstc.com>
2013-02-12 00:44:50 -0800

    Modify assert_true and assert_false tests to allow a pass or fail.

--- tests/identity/false.cpp
+++ tests/identity/false.cpp
@@ -4,6 +4,31 @@
 #include <iostream>
 #include <cstdlib>
 
+auto v1 () -> void {
+  using unittest::v1::assert_false_error;
+  auto self = unittest::v1::self;
+
+  try { self.assert_false(false); }
+  catch (assert_false_error const&) {
+    std::clog << "unexpected assert_false thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  } catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_false(true); }
+  catch (assert_false_error const&) { return; }
+  catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::clog << "assert_false was not thrown" << std::endl;
+  std::exit(EXIT_FAILURE);
+}
+
 auto main () -> int {
-  return EXIT_FAILURE;
+  v1();
+  return EXIT_SUCCESS;
 }
--- tests/identity/true.cpp
+++ tests/identity/true.cpp
@@ -5,10 +5,30 @@
 #include <cstdlib>
 
 auto v1 () -> void {
+  using unittest::v1::assert_true_error;
+  auto self = unittest::v1::self;
 
+  try { self.assert_true(true); }
+  catch (assert_true_error const&) {
+    std::clog << "unexpected assert_true thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  } catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_true(false); }
+  catch (assert_true_error const&) { return; }
+  catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::clog << "assert_true was not thrown" << std::endl;
+  std::exit(EXIT_FAILURE);
 }
 
 auto main () -> int {
   v1();
-  return EXIT_FAILURE;
+  return EXIT_SUCCESS;
 }
