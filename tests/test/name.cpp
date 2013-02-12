b1bae09cf88ff9519841dba7be3aef11dbec45c0
Tres Walsh <tres.walsh@mnmlstc.com>
2013-02-11 22:23:40 -0800

    Modify the 'test' type's unit tests to allow for passes and fails.
      Closes #4

--- tests/test/name.cpp
+++ tests/test/name.cpp
@@ -1,4 +1,7 @@
 #include <unittest/test.hpp>
+#include <unittest/task.hpp>
+#include <unittest/safe.hpp>
+
 #include <iostream>
 #include <cstdlib>
 #include <csignal>
@@ -8,7 +11,22 @@ auto sigsegv (int) -> void {
   std::exit(EXIT_FAILURE);
 }
 
+/* Tests v1 interface for unittest::test */
+auto v1 () -> void {
+
+  unittest::v1::test("my-name") = {
+    unittest::v1::task("nothing") = []{ }
+  };
+
+  auto name = std::get<0>(*unittest::v1::vault.begin());
+  if (name != "my-name") {
+    std::clog << "test did not submit proper name: " << name << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+}
+
 auto main () -> int {
   std::signal(SIGSEGV, sigsegv);
-  return EXIT_FAILURE;
+  v1();
+  return EXIT_SUCCESS;
 }
--- tests/test/run.cpp
+++ tests/test/run.cpp
@@ -1,4 +1,8 @@
 #include <unittest/test.hpp>
+#include <unittest/task.hpp>
+#include <unittest/safe.hpp>
+
+#include <stdexcept>
 #include <iostream>
 #include <cstdlib>
 #include <csignal>
@@ -8,7 +12,35 @@ auto sigsegv (int) -> void {
   std::exit(EXIT_FAILURE);
 }
 
+auto v1 () -> void {
+  unittest::v1::test("my-test") = {
+    unittest::v1::task("1st-task") = [] {
+      throw std::runtime_error("no logic");
+    },
+    unittest::v1::task("2nd-task") = [] {
+      throw std::runtime_error("no logic");
+    }
+  };
+
+  auto value = 0;
+  for (auto item : unittest::v1::vault) {
+    try { std::get<2>(item)(); }
+    catch (std::runtime_error const& e) {
+      value += 1;
+      continue;
+    }
+    std::clog << "task did not throw as expected" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  if (value != 2) {
+    std::clog << "test did not submit proper number of tasks" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+}
+
 auto main () -> int {
   std::signal(SIGSEGV, sigsegv);
-  return EXIT_FAILURE;
+  v1();
+  return EXIT_SUCCESS;
 }
