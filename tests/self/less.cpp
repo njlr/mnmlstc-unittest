c6632a4d65849e8e0fd6170e3fd76d566be3dfd2
Tres Walsh <tres.walsh@mnmlstc.com>
2013-03-01 10:30:40 -0800

    Add self-less unit test implementation

--- tests/self/less.cpp
+++ tests/self/less.cpp
@@ -2,9 +2,48 @@
 #include <unittest/error.hpp>
 
 #include <iostream>
+#include <string>
+
 #include <cstdlib>
 
+struct custom { };
+
+void v1 () {
+  using unittest::v1::exception;
+  using unittest::v1::self;
+
+  try { self.assert_less(1, 2); }
+  catch (...) {
+    std::clog << "unexpected exception was thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_less(1, 1); }
+  catch (exception const& e) {
+    if (std::string { "assert_less" } != e.type()) {
+      std::clog << "unexpected exception '" << e.type() << "' was thrown"
+                << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  } catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_less(custom(), custom()); }
+  catch (exception const& e) {
+    if (std::string { "assert_less" } != e.type()) {
+      std::clog << "unexpected exception '" << e.type() << "' was thrown"
+                << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  } catch (...) {
+    std::clog << "unexpected exception was thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+}
+
 int main () {
-  std::clog << "Not yet implemented" << std::endl;
-  return EXIT_FAILURE;
+  v1();
+  return EXIT_SUCCESS;
 }
