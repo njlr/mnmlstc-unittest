53e0e58314958c6189712ccccf3df43cb6110f08
Tres Walsh <tres.walsh@mnmlstc.com>
2013-03-02 01:00:06 -0800

    Add implementation for self-greater test

--- tests/self/greater.cpp
+++ tests/self/greater.cpp
@@ -2,10 +2,48 @@
 #include <unittest/error.hpp>
 
 #include <iostream>
+#include <string>
+
 #include <cstdlib>
 
-int main () {
-  std::clog << "Not yet implemented" << std::endl;
-  return EXIT_FAILURE;
+struct custom { };
+
+void v1 () {
+  using unittest::v1::exception;
+  using unittest::v1::self;
+
+  try { self.assert_greater(2, 1); }
+  catch (...) {
+    std::clog << "unexpected exception was thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_greater(1, 2); }
+  catch (exception const& e) {
+    if (std::string { "assert_greater" } != e.type()) {
+      std::clog << "unexpected exception '" << e.type() << "' was thrown"
+                << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  } catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_greater(custom(), custom()); }
+  catch (exception const& e) {
+    if (std::string { "assert_greater" } != e.type()) {
+      std::clog << "unexpected exception '" << e.type() << "' was thrown"
+                << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  } catch (...) {
+    std::clog << "unexpected exception was thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
 }
 
+int main () {
+  v1();
+  return EXIT_SUCCESS;
+}
