1332e03d361e3090863156d5acfda1ddd5afeb14
Tres Walsh <tres.walsh@mnmlstc.com>
2013-03-01 17:41:42 -0800

    Add skeleton test for identity::assert_not_almost_equal

---
+++ tests/self/not-almost.cpp
@@ -0,0 +1,12 @@
+#include <unittest/identity.hpp>
+#include <unittest/error.hpp>
+
+#include <iostream>
+#include <string>
+
+#include <cstdlib>
+
+int main () {
+  std::clog << "Not yet implemented" << std::endl;
+  std::exit(EXIT_FAILURE);
+}
