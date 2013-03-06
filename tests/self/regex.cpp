c9cea1c61fed29bc7afe759e08c8244bec782c0c
Tres Walsh <tres.walsh@mnmlstc.com>
2013-03-06 13:07:27 -0800

    Add implementation for identity::assert_regex unit test
    Please note that the internal tests (and by extension *all* assert_regex
    statements) will fail on gcc 4.7.2 until the <regex> header is properly
    implemented.

--- tests/self/regex.cpp
+++ tests/self/regex.cpp
@@ -2,9 +2,37 @@
 #include <unittest/error.hpp>
 
 #include <iostream>
+#include <string>
+
 #include <cstdlib>
 
-int main () {
-  std::clog << "Not yet implemented" << std::endl;
+void v1 () {
+  using unittest::v1::exception;
+  using unittest::v1::self;
+
+  try { self.assert_regex("abcdefg", "[a-z]"); }
+  catch (...) {
+    std::clog << "unexpected exception was thrown: " << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_regex("abcdefg", "[0-9]"); }
+  catch (exception const& e) {
+    if (std::string { "assert_regex" } == e.type()) { return; }
+    std::clog << "unexpected exception '" << e.type() << "' was thrown"
+              << std::endl;
+    std::exit(EXIT_FAILURE);
+  } catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::clog << "expected behavior has not occurred" << std::endl;
   std::exit(EXIT_FAILURE);
+  
+}
+
+int main () {
+  v1();
+  return EXIT_SUCCESS;
 }
