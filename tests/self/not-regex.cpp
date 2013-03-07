07ce4d36534875d7fac89aa441db5132e25a0b46
Tres Walsh <tres.walsh@mnmlstc.com>
2013-03-07 14:30:54 -0800

    Add implementation for identity::assert_not_regex unit test

--- tests/self/not-regex.cpp
+++ tests/self/not-regex.cpp
@@ -4,7 +4,31 @@
 #include <iostream>
 #include <cstdlib>
 
-int main () {
-  std::clog << "Not yet implemented" << std::endl;
+void v1 () {
+  using unittest::v1::exception;
+  using unittest::v1::self;
+
+  try { self.assert_not_regex("abcdefg", "[0-9]"); }
+  catch (...) {
+    std::clog << "unexpected exception was thrown: " << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  try { self.assert_regex("abcdefg", "[a-z]"); }
+  catch (exception const& e) {
+    if (std::string { "assert_not_regex" } == e.type()) { return; }
+    std::clog << "unexpected exception '" << e.type() << "' was thrown"
+              << std::endl;
+  } catch (...) {
+    std::clog << "unexpected exception thrown" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::clog << "expected behavior has not occurred" << std::endl;
   std::exit(EXIT_FAILURE);
+}
+
+int main () {
+  v1();
+  return EXIT_SUCCESS;
 }
